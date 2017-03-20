5d3fe84b9f91e840b2f44f23b107e8f5f5ae1582
liyuanhong <908963295@qq.com>
2017-03-20 23:16:15 +0800

    添加了热榜的遍历测试

--- startTest.py
+++ startTest.py
@@ -1,8 +1,30 @@
 #coding:utf-8
 #Edit by liyuanhong 2016/10/17#
 import sys
-
 curDir = sys.path[0]
+
+
+def delErrorPng():
+	path = sys.path[0]
+	path = path + "/" + "errorScreenShot"
+	#windows下的写法
+	sys.path.append(curDir + '\\common\\FileModule')
+	#mac下的写法
+	sys.path.append(curDir + '/common/FileModule')
+	import FileModule
+	FileModule.delAllFilesOfDir(path)
+
+
+
+
+
+
+
+
+
+
+
+
 
 #windows下的写法
 sys.path.append(curDir + '\\MPTestCases\\traversElesTest')
@@ -12,28 +34,23 @@ sys.path.append(curDir + '/MPTestCases/traversElesTest')
 import TraverseDiscaveryPage
 import TraverseHotPage
 import TraverseMyPage
+import TraverseRebangPage
 
 #执行完所有用例所消耗的时间
 totalTime = 0
 #执行完单条用例所消耗的时间
 singleTime = 0
 
+#删除上传运行的所有截图
+delErrorPng()
 
-'''
-TraverseDiscaveryPage.suite("0")
-TraverseHotPage.suite("0")
-TraverseMyPage.suite("0")
-'''
+#TraverseDiscaveryPage.suite("0")
+#TraverseHotPage.suite("0")
+#TraverseMyPage.suite("0")
+TraverseRebangPage.suite("0")
 
 
 
-path = sys.path[0]
-path = path + "/" + "errorScreenShot"
-#windows下的写法
-sys.path.append(curDir + '\\common\\FileModule')
-#mac下的写法
-sys.path.append(curDir + '/common/FileModule')
-import FileModule
-FileModule.delAllFilesOfDir(path)
+
 
 
